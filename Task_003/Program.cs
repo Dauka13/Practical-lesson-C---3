59b96b9151a7ff328a0c3fdcda3dbfbfeef383cf
Dauka <orazbekovdd13@mail.ru>
2022-08-18 18:02:30 +0600

    add task 003 need find a number in array

---
+++ Task_003/Program.cs
@@ -0,0 +1,42 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] CreatArray(int size)
+{
+    var array = new int[size];
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var element in array)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+bool SearchNumber(int[] array, int find)
+{
+    foreach (var element in array)
+        if (element == find) 
+            return true;
+        return false;
+}
+
+Console.Write("Enter length of array: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] searchArray = CreatArray(size);
+
+Console.WriteLine("Array contains:");
+PrintArray(searchArray);
+
+Console.Write("Enter number to search: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+bool findNumber = SearchNumber(searchArray, number);
+
+Console.WriteLine(findNumber ? $"Number {number} found " :
+                        "This number isn't here.");
