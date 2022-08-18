dc464e8202d62494f55d9c6a15a6f0c505d0bb0a
Dauka <orazbekovdd13@mail.ru>
2022-08-18 17:41:44 +0600

    add task 002 about replace elements on reverse elements

---
+++ Task_002/Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+int[] CreateArray(int size)
+{
+    var array = new int [size];
+    var random = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-9, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+int[] Replace(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+Console.Clear();
+Console.Write("Enter length of array: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[] Numbers = CreateArray(length);
+
+Console.WriteLine("Given array: ");
+PrintArray(Numbers);
+
+int [] Numbers2 = Replace(Numbers);
+Console.WriteLine("Reverse array: ");
+PrintArray(Numbers2);
