aa8838bcf22eca76241813960b8a4ade544c0fe3
Dauka <orazbekovdd13@mail.ru>
2022-08-18 19:09:39 +0600

    add task 005 about elements in segment [10, 99]

---
+++ Task_005/Program.cs
@@ -0,0 +1,43 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] CreatArray()
+{
+    int size = 123;
+    var array = new int[size];
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(0, 1000);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var element in array)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+int AmountOfNumbers(int[] array)
+{
+    int quantity = 0;
+    foreach(var element in array)
+        if(element >= 10 && element <= 99) quantity++;
+    return quantity;
+}
+
+bool AllNumbers(int amount)
+{
+    if(amount > 0) return true;
+    else return false;
+}
+
+Console.Clear();
+int[] numbers = CreatArray();
+Console.WriteLine("Array");
+PrintArray(numbers);
+
+int sum = AmountOfNumbers(numbers);
+
+bool Digit = AllNumbers(sum);
+Console.WriteLine(Digit ? $"Amount of elements in segment [10, 99]: {sum}" 
+                : "There is no elements in segment [10, 99]");
